d7b0dc7079efccf65d00f76ff45849914653f096
Kacper <kacperniwczyk@gmail.com>
2021-03-06 17:59:20 +0100

    Added functionalities to event reservation

--- HotelServiceSystem/Entities/EventReservation.cs
+++ HotelServiceSystem/Entities/EventReservation.cs
@@ -1,7 +1,27 @@
-﻿namespace HotelServiceSystem.Entities
+﻿using System.Linq;
+using HotelServiceSystem.ViewModel;
+
+namespace HotelServiceSystem.Entities
 {
     public class EventReservation : Reservation
     {
         public string Description { get; set; }
+        
+        public void UpdateFrom(EventReservationViewModel reservationViewModel)
+        {
+            Client = reservationViewModel.Client;
+            NumberOfGuests = reservationViewModel.NumberOfGuests;
+            DateFrom = reservationViewModel.DateFrom;
+            DateTo = reservationViewModel.DateTo;
+            Price = reservationViewModel.Price;
+            DateOfSubmission = reservationViewModel.DateOfSubmission;
+            Discount = reservationViewModel.Discount;
+            HasFinished = reservationViewModel.HasFinished;
+            RoomReservations = reservationViewModel.SelectedRooms.Select(x => new RoomReservation() {Room = x, Reservation = this})
+                .ToList();
+            AdditionalServiceReservations = reservationViewModel.SelectedAdditionalServices.Select(x => new AdditionalServiceReservation() {AdditionalService = x, Reservation = this})
+                .ToList();
+            Description = reservationViewModel.Description;
+        }
     }
 }
