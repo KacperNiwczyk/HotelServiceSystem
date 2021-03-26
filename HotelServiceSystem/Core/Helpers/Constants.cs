6d413002423bfeb85ef129d74f43e15b0d937aa9
Kacper <kacperniwczyk@gmail.com>
2021-03-26 14:18:44 +0100

    Added more tabels

---
+++ HotelServiceSystem/Core/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelServiceSystem.Core.Helpers
+{
+	public static class Constants
+	{
+		public const string DefaultDateFormat = "dd/MM/yy";
+
+		public const string DefaultDateTimeFormat = "dd/MM/yy HH:mm:ss";
+	}
+}
